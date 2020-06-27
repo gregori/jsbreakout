9c70a3cff386df4eb41dbee43df74d870b88ca23
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2020-06-26 22:29:33 -0300

    mostrando tijolos

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+// bolinha
 let x = canvas.width / 2; // inicial horizontal
 let y = canvas.height - 35; // inicial vertical
 let dx = 2; // variação horizontal
 let dy = -2; // variação vertical;
 let ballRadius = 10; // raio da bola
 
+// barra
 let paddleHeight = 10;
 let paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let paddleY = (canvas.height - paddleHeight) - 10;
 let rightPressed = false;
 let leftPressed = false;
+
+// pedras
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
+
+function drawBrick(brickX, brickY) {
+  ctx.beginPath();
+  ctx.rect(brickX, brickY, brickWidth, brickHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+      let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      drawBrick(brickX, brickY);
+    }
+  }
+}
 
 function drawPaddle() {
   ctx.beginPath();
@@ -34,14 +73,14 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
+  drawBricks();
   // verifica se a bola sai na horizontal
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx; // inverte o sinal de dx
   }
   // verifica se a bola sai na vertical
-  if (y + dy < ballRadius) {
-    dy = -dy;
-  } else if (x > paddleX && x < paddleX + paddleY &&
+  if (y + dy < ballRadius ||
+    x > paddleX && x < paddleX + paddleWidth && // entre a barra (eixo x)
     y + ballRadius >= paddleY) {
     dy = -dy;
   } else if (y + dy > canvas.height - ballRadius) {
